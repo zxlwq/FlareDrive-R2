3c4f95d26e5b6f4566bc536f01480f93382a13f7
LiuShen <3162475700@qq.com>
2025-03-12 13:37:12 +0800

    😋美化样式，修改卡片圆角，实现更加美观的效果

--- assets/App.vue
+++ assets/App.vue
@@ -1,152 +1,102 @@
 <template>
   <div class="main" @dragenter.prevent @dragover.prevent @drop.prevent="onDrop">
-    <progress
-      v-if="uploadProgress !== null"
-      :value="uploadProgress"
-      max="100"
-    ></progress>
-    <UploadPopup
-      v-model="showUploadPopup"
-      @upload="onUploadClicked"
-      @createFolder="createFolder"
-    ></UploadPopup>
+    <progress v-if="uploadProgress !== null" :value="uploadProgress" max="100"></progress>
+    <UploadPopup v-model="showUploadPopup" @upload="onUploadClicked" @createFolder="createFolder"></UploadPopup>
     <button class="upload-button circle" @click="showUploadPopup = true">
-      <img
-        style="filter: invert(100%)"
+      <img style="filter: invert(100%)"
         src="https://cdnjs.cloudflare.com/ajax/libs/material-design-icons/4.0.0/png/file/upload_file/materialicons/36dp/2x/baseline_upload_file_black_36dp.png"
-        alt="Upload"
-        width="36"
-        height="36"
-        @contextmenu.prevent
-      />
+        alt="Upload" width="36" height="36" @contextmenu.prevent />
     </button>
     <div class="app-bar">
       <input type="search" v-model="search" aria-label="Search" />
       <div class="menu-button">
         <button class="circle" @click="showMenu = true">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 448 512"
-            width="24"
-            height="24"
-            title="Menu"
-            style="display: block; margin: 4px"
-          >
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width="24" height="24" title="Menu"
+            style="display: block; margin: 4px">
             <!--! Font Awesome Pro 6.2.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
             <path
-              d="M120 256c0 30.9-25.1 56-56 56s-56-25.1-56-56s25.1-56 56-56s56 25.1 56 56zm160 0c0 30.9-25.1 56-56 56s-56-25.1-56-56s25.1-56 56-56s56 25.1 56 56zm104 56c-30.9 0-56-25.1-56-56s25.1-56 56-56s56 25.1 56 56s-25.1 56-56 56z"
-            />
+              d="M120 256c0 30.9-25.1 56-56 56s-56-25.1-56-56s25.1-56 56-56s56 25.1 56 56zm160 0c0 30.9-25.1 56-56 56s-56-25.1-56-56s25.1-56 56-56s56 25.1 56 56zm104 56c-30.9 0-56-25.1-56-56s25.1-56 56-56s56 25.1 56 56s-25.1 56-56 56z" />
           </svg>
         </button>
-        <Menu
-          v-model="showMenu"
-          :items="[{ text: '名称A-Z' }, { text: '大小↑' } ,{ text: '大小↓' }, { text: '粘贴' }]"
-          @click="onMenuClick"
-        />
+        <Menu v-model="showMenu" :items="[{ text: '名称A-Z' }, { text: '大小↑' }, { text: '大小↓' }, { text: '粘贴' }]"
+          @click="onMenuClick" />
       </div>
     </div>
-    <ul class="file-list">
-      <li v-if="cwd !== ''">
-        <div
-          tabindex="0"
-          class="file-item"
-          @click="cwd = cwd.replace(/[^\/]+\/$/, '')"
-          @contextmenu.prevent
-        >
-          <div class="file-icon">
-            <img
-              src="https://cdnjs.cloudflare.com/ajax/libs/material-design-icons/4.0.0/png/file/folder/materialicons/36dp/2x/baseline_folder_black_36dp.png"
-              width="36"
-              height="36"
-              alt="Folder"
-            />
+    <div class="file-list-container">
+      <ul class="file-list">
+        <li v-if="cwd !== ''">
+          <div tabindex="0" class="file-item" @click="cwd = cwd.replace(/[^\/]+\/$/, '')" @contextmenu.prevent>
+            <div class="file-icon">
+              <img
+                src="https://cdnjs.cloudflare.com/ajax/libs/material-design-icons/4.0.0/png/file/folder/materialicons/36dp/2x/baseline_folder_black_36dp.png"
+                width="36" height="36" alt="Folder" />
+            </div>
+            <span class="file-name">..</span>
           </div>
-          <span class="file-name">..</span>
-        </div>
-      </li>
-      <li v-for="folder in filteredFolders" :key="folder">
-        <div
-          tabindex="0"
-          class="file-item"
-          @click="cwd = folder"
-          @contextmenu.prevent="
+        </li>
+        <li v-for="folder in filteredFolders" :key="folder">
+          <div tabindex="0" class="file-item" @click="cwd = folder" @contextmenu.prevent="
             showContextMenu = true;
-            focusedItem = folder;
-          "
-        >
-          <div class="file-icon">
-            <img
-              src="https://cdnjs.cloudflare.com/ajax/libs/material-design-icons/4.0.0/png/file/folder/materialicons/36dp/2x/baseline_folder_black_36dp.png"
-              width="36"
-              height="36"
-              alt="Folder"
-            />
-          </div>
-          <span
-            class="file-name"
-            v-text="folder.match(/.*?([^/]*)\/?$/)[1]"
-          ></span>
-          <div style="margin-right: 10px;margin-left: auto;"
-            @click.stop="
+          focusedItem = folder;
+          ">
+            <div class="file-icon">
+              <img
+                src="https://cdnjs.cloudflare.com/ajax/libs/material-design-icons/4.0.0/png/file/folder/materialicons/36dp/2x/baseline_folder_black_36dp.png"
+                width="36" height="36" alt="Folder" />
+            </div>
+            <span class="file-name" v-text="folder.match(/.*?([^/]*)\/?$/)[1]"></span>
+            <div style="margin-right: 10px;margin-left: auto;" @click.stop="
               showContextMenu = true;
-              focusedItem = folder;
-            "
-            >
-              <svg viewBox="0 0 24 24" style="height: 30px; width: 30px;"><path fill="currentColor" d="M10.5,12A1.5,1.5 0 0,1 12,10.5A1.5,1.5 0 0,1 13.5,12A1.5,1.5 0 0,1 12,13.5A1.5,1.5 0 0,1 10.5,12M10.5,16.5A1.5,1.5 0 0,1 12,15A1.5,1.5 0 0,1 13.5,16.5A1.5,1.5 0 0,1 12,18A1.5,1.5 0 0,1 10.5,16.5M10.5,7.5A1.5,1.5 0 0,1 12,6A1.5,1.5 0 0,1 13.5,7.5A1.5,1.5 0 0,1 12,9A1.5,1.5 0 0,1 10.5,7.5M12,2A10,10 0 0,1 22,12A10,10 0 0,1 12,22A10,10 0 0,1 2,12A10,10 0 0,1 12,2M12,4A8,8 0 0,0 4,12A8,8 0 0,0 12,20A8,8 0 0,0 20,12A8,8 0 0,0 12,4Z"></path></svg>
+            focusedItem = folder;
+            ">
+              <svg viewBox="0 0 24 24" style="height: 30px; width: 30px;">
+                <path fill="currentColor"
+                  d="M10.5,12A1.5,1.5 0 0,1 12,10.5A1.5,1.5 0 0,1 13.5,12A1.5,1.5 0 0,1 12,13.5A1.5,1.5 0 0,1 10.5,12M10.5,16.5A1.5,1.5 0 0,1 12,15A1.5,1.5 0 0,1 13.5,16.5A1.5,1.5 0 0,1 12,18A1.5,1.5 0 0,1 10.5,16.5M10.5,7.5A1.5,1.5 0 0,1 12,6A1.5,1.5 0 0,1 13.5,7.5A1.5,1.5 0 0,1 12,9A1.5,1.5 0 0,1 10.5,7.5M12,2A10,10 0 0,1 22,12A10,10 0 0,1 12,22A10,10 0 0,1 2,12A10,10 0 0,1 12,2M12,4A8,8 0 0,0 4,12A8,8 0 0,0 12,20A8,8 0 0,0 20,12A8,8 0 0,0 12,4Z">
+                </path>
+              </svg>
+            </div>
           </div>
-        </div>
-      </li>
-      <li v-for="file in filteredFiles" :key="file.key">
-        <div
-          @click="preview(`/raw/${file.key}`)"
-          @contextmenu.prevent="
+        </li>
+        <li v-for="file in filteredFiles" :key="file.key">
+          <div @click="preview(`/raw/${file.key}`)" @contextmenu.prevent="
             showContextMenu = true;
-            focusedItem = file;
-          "
-        >
-          <div class="file-item">
-            <MimeIcon
-              :content-type="file.httpMetadata.contentType"
-              :thumbnail="
-                file.customMetadata.thumbnail
+          focusedItem = file;
+          ">
+            <div class="file-item">
+              <MimeIcon :content-type="file.httpMetadata.contentType" :thumbnail="file.customMetadata.thumbnail
                   ? `/raw/_$flaredrive$/thumbnails/${file.customMetadata.thumbnail}.png`
                   : null
-              "
-            />
-            <div>
-              <div class="file-name" v-text="file.key.split('/').pop()"></div>
-              <div class="file-attr">
-                <span v-text="new Date(file.uploaded).toLocaleString()"></span>
-                <span v-text="formatSize(file.size)"></span>
+                " />
+              <div>
+                <div class="file-name" v-text="file.key.split('/').pop()"></div>
+                <div class="file-attr">
+                  <span v-text="new Date(file.uploaded).toLocaleString()"></span>
+                  <span v-text="formatSize(file.size)"></span>
+                </div>
+              </div>
+              <div style="margin-right: 10px;margin-left: auto;" @click.stop="
+                showContextMenu = true;
+              focusedItem = file;
+              ">
+                <svg viewBox="0 0 24 24" style="height: 30px; width: 30px;">
+                  <path fill="currentColor"
+                    d="M10.5,12A1.5,1.5 0 0,1 12,10.5A1.5,1.5 0 0,1 13.5,12A1.5,1.5 0 0,1 12,13.5A1.5,1.5 0 0,1 10.5,12M10.5,16.5A1.5,1.5 0 0,1 12,15A1.5,1.5 0 0,1 13.5,16.5A1.5,1.5 0 0,1 12,18A1.5,1.5 0 0,1 10.5,16.5M10.5,7.5A1.5,1.5 0 0,1 12,6A1.5,1.5 0 0,1 13.5,7.5A1.5,1.5 0 0,1 12,9A1.5,1.5 0 0,1 10.5,7.5M12,2A10,10 0 0,1 22,12A10,10 0 0,1 12,22A10,10 0 0,1 2,12A10,10 0 0,1 12,2M12,4A8,8 0 0,0 4,12A8,8 0 0,0 12,20A8,8 0 0,0 20,12A8,8 0 0,0 12,4Z">
+                  </path>
+                </svg>
               </div>
             </div>
-            <div style="margin-right: 10px;margin-left: auto;"
-            @click.stop="
-              showContextMenu = true;
-              focusedItem = file;
-            "
-            >
-              <svg viewBox="0 0 24 24" style="height: 30px; width: 30px;"><path fill="currentColor" d="M10.5,12A1.5,1.5 0 0,1 12,10.5A1.5,1.5 0 0,1 13.5,12A1.5,1.5 0 0,1 12,13.5A1.5,1.5 0 0,1 10.5,12M10.5,16.5A1.5,1.5 0 0,1 12,15A1.5,1.5 0 0,1 13.5,16.5A1.5,1.5 0 0,1 12,18A1.5,1.5 0 0,1 10.5,16.5M10.5,7.5A1.5,1.5 0 0,1 12,6A1.5,1.5 0 0,1 13.5,7.5A1.5,1.5 0 0,1 12,9A1.5,1.5 0 0,1 10.5,7.5M12,2A10,10 0 0,1 22,12A10,10 0 0,1 12,22A10,10 0 0,1 2,12A10,10 0 0,1 12,2M12,4A8,8 0 0,0 4,12A8,8 0 0,0 12,20A8,8 0 0,0 20,12A8,8 0 0,0 12,4Z"></path></svg>
-            </div>
           </div>
-        </div>
-      </li>
-    </ul>
+        </li>
+      </ul>
+    </div>
     <div v-if="loading" style="margin-top: 12px; text-align: center">
       <span>加载中...</span>
     </div>
-    <div
-      v-else-if="!filteredFiles.length && !filteredFolders.length"
-      style="margin-top: 12px; text-align: center"
-    >
+    <div v-else-if="!filteredFiles.length && !filteredFolders.length" style="margin-top: 12px; text-align: center">
       <span>没有文件</span>
     </div>
     <Dialog v-model="showContextMenu">
-      <div
-        v-text="focusedItem.key || focusedItem"
-        class="contextmenu-filename"
-        @click.stop.prevent
-      ></div>
+      <div v-text="focusedItem.key || focusedItem" class="contextmenu-filename" @click.stop.prevent></div>
       <ul v-if="typeof focusedItem === 'string'" class="contextmenu-list">
         <li>
           <button @click="copyLink(`/?p=${encodeURIComponent(focusedItem)}`)">
@@ -159,10 +109,7 @@
           </button>
         </li>
         <li>
-          <button
-            style="color: red"
-            @click="removeFile(focusedItem + '_$folder$')"
-          >
+          <button style="color: red" @click="removeFile(focusedItem + '_$folder$')">
             <span>删除</span>
           </button>
         </li>
@@ -278,7 +225,7 @@ export default {
           .then((value) => {
             if (value.redirected) window.location.href = value.url;
           })
-          .catch(() => {});
+          .catch(() => { });
         console.log(`Create folder failed`);
       }
     },
@@ -355,7 +302,7 @@ export default {
       fileElement.value = null;
     },
 
-    preview(filePath){
+    preview(filePath) {
       window.open(filePath);
     },
 
@@ -393,7 +340,7 @@ export default {
               .then((value) => {
                 if (value.redirected) window.location.href = value.url;
               })
-              .catch(() => {});
+              .catch(() => { });
             console.log(`Upload ${digestHex}.png failed`);
           }
         } catch (error) {
@@ -423,7 +370,7 @@ export default {
           .then((value) => {
             if (value.redirected) window.location.href = value.url;
           })
-          .catch(() => {});
+          .catch(() => { });
         console.log(`Upload ${file.name} failed`, error);
       }
       setTimeout(this.processUploadQueue);
@@ -447,7 +394,7 @@ export default {
       // 获取当前的目录结构
       const currentPath = this.cwd; // 当前所在目录
       const allFolders = [...this.folders]; // 所有可用目录
-      
+
       // 如果不在根目录，添加返回上级目录选项
       if (currentPath !== '') {
         const parentPath = currentPath.replace(/[^\/]+\/$/, '');
@@ -455,49 +402,49 @@ export default {
           allFolders.unshift(parentPath);
         }
       }
-      
+
       // 添加根目录选项
       if (!allFolders.includes('')) {
         allFolders.unshift('');
       }
-      
+
       // 构建选择列表
       const folderOptions = allFolders.map(folder => {
-        const displayName = folder === '' ? '根目录' : 
-                          folder === currentPath ? '当前目录' :
-                          folder.replace(/.*\/(?!$)|\//g, '') + '/';
+        const displayName = folder === '' ? '根目录' :
+          folder === currentPath ? '当前目录' :
+            folder.replace(/.*\/(?!$)|\//g, '') + '/';
         return {
           display: displayName,
           value: folder
         };
       });
-      
+
       // 创建选择提示
-      const options = folderOptions.map((opt, index) => 
+      const options = folderOptions.map((opt, index) =>
         `${index + 1}. ${opt.display}`
       ).join('\n');
-      
+
       const promptText = `请选择目标目录(输入数字):\n${options}\n`;
       const selection = window.prompt(promptText);
-      
+
       if (!selection) return;
-      
+
       const selectedIndex = parseInt(selection) - 1;
       if (isNaN(selectedIndex) || selectedIndex < 0 || selectedIndex >= folderOptions.length) {
         alert('无效的选择');
         return;
       }
-      
+
       const targetPath = folderOptions[selectedIndex].value;
-      
+
       // 获取文件名
       const fileName = key.split('/').pop();
       // 如果是文件夹,需要移除_$folder$后缀
       const finalFileName = fileName.endsWith('_$folder$') ? fileName.slice(0, -9) : fileName;
-      
+
       // 修复：正确处理目标路径，避免双斜杠
       const normalizedPath = targetPath === '' ? '' : (targetPath.endsWith('/') ? targetPath : targetPath + '/');
-      
+
       try {
         // 如果是目录（以_$folder$结尾），则需要移动整个目录内容
         if (key.endsWith('_$folder$')) {
@@ -505,25 +452,25 @@ export default {
           const sourceBasePath = key.slice(0, -9);
           // 获取目标目录的基础路径，修复根目录的情况
           const targetBasePath = normalizedPath + finalFileName + '/';
-          
+
           // 递归获取所有子文件和子目录
           const allItems = await this.getAllItems(sourceBasePath);
-          
+
           // 显示进度提示
           const totalItems = allItems.length;
           let processedItems = 0;
-          
+
           // 移动所有项目
           for (const item of allItems) {
             const relativePath = item.key.substring(sourceBasePath.length);
             const newPath = targetBasePath + relativePath;
-            
+
             try {
               // 复制到新位置
               await this.copyPaste(item.key, newPath);
               // 删除原位置
               await axios.delete(`/api/write/items/${item.key}`);
-              
+
               // 更新进度
               processedItems++;
               this.uploadProgress = (processedItems / totalItems) * 100;
@@ -531,12 +478,12 @@ export default {
               console.error(`移动 ${item.key} 失败:`, error);
             }
           }
-          
+
           // 移动目录标记
           const targetFolderPath = targetBasePath.slice(0, -1) + '_$folder$';
           await this.copyPaste(key, targetFolderPath);
           await axios.delete(`/api/write/items/${key}`);
-          
+
           // 清除进度
           this.uploadProgress = null;
         } else {
@@ -545,7 +492,7 @@ export default {
           await this.copyPaste(key, targetFilePath);
           await axios.delete(`/api/write/items/${key}`);
         }
-        
+
         // 刷新文件列表
         this.fetchFiles();
       } catch (error) {
@@ -558,19 +505,19 @@ export default {
     async getAllItems(prefix) {
       const items = [];
       let marker = null;
-      
+
       do {
         const url = new URL(`/api/children/${prefix}`, window.location.origin);
         if (marker) {
           url.searchParams.set('marker', marker);
         }
-        
+
         const response = await fetch(url);
         const data = await response.json();
-        
+
         // 添加文件
         items.push(...data.value);
-        
+
         // 处理子目录
         for (const folder of data.folders) {
           // 添加目录标记
@@ -579,15 +526,15 @@ export default {
             size: 0,
             uploaded: new Date().toISOString(),
           });
-          
+
           // 递归获取子目录内容
           const subItems = await this.getAllItems(folder);
           items.push(...subItems);
         }
-        
+
         marker = data.marker;
       } while (marker);
-      
+
       return items;
     },
 
@@ -614,9 +561,8 @@ export default {
             : url.searchParams.delete("p");
           window.history.pushState(null, "", url.toString());
         }
-        document.title = `${
-          this.cwd.replace(/.*\/(?!$)|\//g, "") || "/"
-        } - 文件库`;
+        document.title = `${this.cwd.replace(/.*\/(?!$)|\//g, "") || "/"
+          } - 文件库`;
       },
       immediate: true,
     },
@@ -642,6 +588,7 @@ export default {
 <style>
 .main {
   height: 100%;
+  background-image: url(/bg-light.webp);
 }
 
 .app-bar {
@@ -658,11 +605,30 @@ export default {
   margin-left: 4px;
 }
 
-.menu-button > button {
+.file-list-container {
+  margin: 20px auto;
+  padding: 20px;
+  width: 60%;
+  max-width: 95%;
+  background: rgba(255, 255, 255, 0.8);
+  backdrop-filter: blur(10px);
+  border-radius: 10px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  transition: width 0.3s ease;
+}
+
+@media (max-width: 768px) {
+  .file-list-container {
+    width: 95%;
+    padding: 10px;
+  }
+}
+
+.menu-button>button {
   transition: background-color 0.2s ease;
 }
 
-.menu-button > button:hover {
+.menu-button>button:hover {
   background-color: whitesmoke;
 }
 
