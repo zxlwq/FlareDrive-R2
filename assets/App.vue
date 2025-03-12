d650f2339f89ea39ae1d798f549ae631f899a06e
LiuShen <3162475700@qq.com>
2025-03-13 00:21:18 +0800

    😯尝试适配网站标题

--- assets/App.vue
+++ assets/App.vue
@@ -589,7 +589,7 @@ export default {
             : url.searchParams.delete("p");
           window.history.pushState(null, "", url.toString());
         }
-        document.title = this.cwd === "/" 
+        document.title = this.cwd.replace(/.*\/(?!$)|\//g, "") === "/" 
             ? "FlareDrive-R2 - 优雅的 Cloudflare R2 网盘文件库"
             :`${this.cwd.replace(/.*\/(?!$)|\//g, "") || "/" } - 优雅的 Cloudflare R2 网盘文件库`;
       },
