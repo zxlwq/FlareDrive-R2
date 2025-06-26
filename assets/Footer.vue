952c8b564e498aa832b5054039e7684f6a0acd6d
科技-刘 <165335155+zxlwq@users.noreply.github.com>
2025-06-27 05:08:41 +0800

    Update Footer.vue

--- assets/Footer.vue
+++ assets/Footer.vue
@@ -4,7 +4,7 @@
       <a href="https://home.zxlwq.dpdns.org/" target="_blank" rel="noopener noreferrer" class="icon-link" title="个人主页">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M575.8 255.5c0 18-15 32.1-32 32.1l-32 0 .7 160.2c0 2.7-.2 5.4-.5 8.1l0 16.2c0 22.1-17.9 40-40 40l-16 0c-1.1 0-2.2 0-3.3-.1c-1.4 .1-2.8 .1-4.2 .1L416 512l-24 0c-22.1 0-40-17.9-40-40l0-24 0-64c0-17.7-14.3-32-32-32l-64 0c-17.7 0-32 14.3-32 32l0 64 0 24c0 22.1-17.9 40-40 40l-24 0-31.9 0c-1.5 0-3-.1-4.5-.2c-1.2 .1-2.4 .2-3.6 .2l-16 0c-22.1 0-40-17.9-40-40l0-112c0-.9 0-1.9 .1-2.8l0-69.7-32 0c-18 0-32-14-32-32.1c0-9 3-17 10-24L266.4 8c7-7 15-8 22-8s15 2 21 7L564.8 231.5c8 7 12 15 11 24z"></path></svg>
       </a>
-      <a href="https://blog.zxlwq.dpdns.org/" target="_blank" rel="noopener noreferrer" class="icon-link" title="站长博客">
+      <a href="https://cdn.zxlwq.dpdns.org/" target="_blank" rel="noopener noreferrer" class="icon-link" title="站长博客">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M192 32c0 17.7 14.3 32 32 32c123.7 0 224 100.3 224 224c0 17.7 14.3 32 32 32s32-14.3 32-32C512 128.9 383.1 0 224 0c-17.7 0-32 14.3-32 32zm0 96c0 17.7 14.3 32 32 32c70.7 0 128 57.3 128 128c0 17.7 14.3 32 32 32s32-14.3 32-32c0-106-86-192-192-192c-17.7 0-32 14.3-32 32zM96 144c0-26.5-21.5-48-48-48S0 117.5 0 144L0 368c0 79.5 64.5 144 144 144s144-64.5 144-144s-64.5-144-144-144l-16 0 0 96 16 0c26.5 0 48 21.5 48 48s-21.5 48-48 48s-48-21.5-48-48l0-224z"/></svg>
       </a>
       <a href="https://github.com/zxlwq" target="_blank" rel="noopener noreferrer" class="icon-link" title="GitHub">
