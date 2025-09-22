30258d5f34deee3bfa8198f088bc599dd547d73a
科技-刘 <165335155+zxlwq@users.noreply.github.com>
2025-09-22 09:07:20 +0800

    Update App.vue

--- assets/App.vue
+++ assets/App.vue
@@ -210,7 +210,7 @@ export default {
     showUploadPopup: false,
     uploadProgress: null,
     uploadQueue: [],
-    backgroundImageUrl: "/assets/bg-light.webp"
+    backgroundImageUrl: "/assets/background.webp"
   }),
 
   computed: {
